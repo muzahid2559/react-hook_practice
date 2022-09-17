c7d2ddba3b7d33cb416e5fb06e325882f2923a82
muzahid626 <muzahidul15-2559@diu.edu.bd>
2022-09-17 21:10:39 +0600

    Using Array Destructuring

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,19 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-const Test = props  => {
-    const testState = useState("");
-    // console.log(testState);
-    // console.log(testState[0]);
-    // console.log(testState[1]);
-    if(testState[0] === "") {
-        testState[1]("muzahid");
+const Test = props => {
+    const [myName, setMyName] = useState("");
+    const [myInfo, setMyInfo] = useState({});
+
+    const changeState = () => {
+        setMyName("Rahim");
+        setMyInfo({
+            age: 30,
+            address: "Dhaka, Bangladesh"
+        })
     }
-
     return (
         <div>
-            <h3>{testState[0]}</h3>
+            <button onClick={changeState}>Show</button>
+            <h3>{myName}</h3>
+            <h4>{myInfo.age}</h4>
+            <h4>{myInfo.address}</h4>
         </div>
     );
-};
+}
 
 export default Test;
